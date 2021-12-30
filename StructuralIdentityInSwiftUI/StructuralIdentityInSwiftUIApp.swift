9d8adccffd4da883131e442a75f968a9cb8bd47a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-12-30 19:33:22 +0200

    Initial Commit

---
+++ StructuralIdentityInSwiftUI/StructuralIdentityInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructuralIdentityInSwiftUIApp.swift
+//  StructuralIdentityInSwiftUI
+//
+//  Created by ramil on 30.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct StructuralIdentityInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
